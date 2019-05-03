4f27129a575fd5fbd2e2477bb5656bdc86a87b4b
Ashwin Shenai <ashwins@iitk.ac.in>
2019-05-03 16:50:02 +0530

    fsm adding ros framework

--- imav_planner/src/planner_test.cxx
+++ imav_planner/src/planner_test.cxx
@@ -3,41 +3,47 @@
 int main(int argc, char **argv)
 {
     ros::init(argc,argv,"planner_test");
-    
+
+    ros::NodeHandle nh;
+
+    ros::Subscriber mav_pose_sub = nh.subscribe("odometry", 1, mav_pose_cb_);
+
+    ros::Subscriber gripper_status_sub = nh.subscribe("gripper_status", 1,gripper_status_cb_ );
     state_machine::test_fsm_ machine;
-    machine.start();
-    machine.process_event(state_machine::CmdTakeOff()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdTrajectory()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdHover()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdGetPkg()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdHover()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdDescent()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdLand()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdTakeOff()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdGotoDrop()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdHover()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdDescent()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdDrop()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdDropOver()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdAscent()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdGetPkg()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdHover());state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdDescent()); state_machine::curr_state(machine);
-    machine.process_event(state_machine::CmdLand()); state_machine::curr_state(machine);
-    std::cout<<"End Mission"<<std::endl;
-    machine.stop();
+   
     ros::Rate loopRate(10);
-    
+    // bool machine_on = false;
 
-    while(ros::ok())
-    {
-        geometry_msgs::PoseStamped mav_cmd_pose_;
-        std_msgs::Bool gripper_status_;
-
-        magnus_publisher.publish(mav_cmd_pose_);
-        ros::spinOnce();
-        loopRate.sleep();
-    }
-    
+    // if(!machine_on){
+        machine.start();
+        // machine_on=true;
+        machine.process_event(state_machine::CmdTakeOff(nh)); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdTrajectory()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdHover()); state_machine::curr_state(machine);
+        machine.process_event(state_machine::CmdGetPkg(nh)); state_machine::curr_state(machine);
+        // while(!machine.ReachedLZ(state_machine::CmdHover(nh)))
+        // {
+            machine.process_event(state_machine::CmdHover(nh)); state_machine::curr_state(machine);
+        // }
+    // }
+    // if(machine_on && state_machine::AtLZ){
+        // // machine.process_event(state_machine::CmdHover()); state_machine::curr_state(machine);
+        machine.process_event(state_machine::CmdDescent(nh)); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdLand()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdTakeOff(nh)); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdGotoDrop(nh)); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdHover()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdDescent()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdDrop()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdDropOver()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdAscent()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdGetPkg(nh)); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdHover());state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdDescent()); state_machine::curr_state(machine);
+        // machine.process_event(state_machine::CmdLand()); state_machine::curr_state(machine);
+        std::cout<<"End Mission"<<std::endl;
+        machine.stop();
+        // machine_on = false;
+    // }
     return 0;
 }
--- imav_planner/src/planner_test.h
+++ imav_planner/src/planner_test.h
@@ -3,6 +3,7 @@
 #include<geometry_msgs/PoseStamped.h>
 #include<std_msgs/Bool.h>
 #include<imav_planner/task_info.h>
+#include<std_srvs/Empty.h>
 
 #include <boost/msm/back/state_machine.hpp>
 #include <boost/msm/back/mpl_graph_fsm_check.hpp>
@@ -10,6 +11,8 @@
 #include <boost/msm/front/functor_row.hpp>
 #include <boost/msm/front/euml/common.hpp>
 #include <boost/msm/front/euml/operator.hpp>
+
+#define E 0.2
 
 nav_msgs::Odometry mav_pose_;
 imav_planner::task_info task_info_;
@@ -30,28 +33,64 @@ void gripper_status_cb_(const std_msgs::Bool &msg)
     gripper_status_ = msg;
 }
 
-ros::NodeHandle nh;
-
-ros::Subscriber mav_pose_sub = nh.subscribe("odometry", 1, mav_pose_cb_);
-ros::Subscriber task_info_sub = nh.subscribe("task_info", 1, task_info_cb_);
-ros::Subscriber gripper_status_sub = nh.subscribe("gripper_status", 1,gripper_status_cb_ );
-
 namespace state_machine 
 {
 
     namespace msm = boost::msm;
     namespace mpl = boost::mpl;
 
-    struct CmdTakeOff{};
+    struct CmdTakeOff
+    {
+        ros::NodeHandle nh;
+        CmdTakeOff(ros::NodeHandle nh_):nh(nh_){}
+    };
+
     struct CmdTrajectory{};
-    struct CmdGetPkg{};
-    struct CmdDescent{};
-    struct CmdGotoDrop{};
-    struct CmdAscent{};
+
+    struct CmdGetPkg
+    {
+        ros::NodeHandle nh;
+        CmdGetPkg(ros::NodeHandle nh_):nh(nh_){}
+    };
+
+    struct CmdDescent
+    {
+        ros::NodeHandle nh;
+        CmdDescent(ros::NodeHandle nh_):nh(nh_){}
+    };
+
+    struct CmdGotoDrop
+    {
+        ros::NodeHandle nh;
+        CmdGotoDrop(ros::NodeHandle nh_):nh(nh_){}
+    };
+
+    struct CmdAscent
+    {
+        ros::NodeHAndle nh;
+        CmdAscent(ros::NodeHandle nh_):nh(nh_){}
+    };
+
     struct CmdDrop{};
-    struct CmdLand{};
+
+    struct CmdLand
+    {
+        ros::NodeHandle nh;
+        CmdLand(ros::NodeHandle nh_):nh(nh_){}
+    };
+
     struct CmdDropOver{};
-    struct CmdHover{};
+    
+    struct CmdHover
+    {
+        ros::NodeHandle nh;
+        CmdHover(ros::NodeHandle nh_):nh(nh_){}
+    };
+
+    bool PkgAttached = false;
+    bool ExploreDone = false;
+    bool AtLZ = false;
+    bool AtMailbox = false;
 
     struct test_fsm : public msm::front::state_machine_def <test_fsm>
     {
@@ -174,49 +213,250 @@ namespace state_machine
         };
 
         typedef Rest initial_state;
-        bool PkgAttached = false;
-        bool ExploreDone = false;
-        bool AtLZ = false;
-        bool AtMailbox = false;
 
         // void start_printing(print const &) { std::cout << "start_printet" << std::endl; }
-        void TakeOff(CmdTakeOff const &) 
+        void TakeOff(CmdTakeOff const & cmd) 
         { 
-            
-            std::cout << "Taking off" << std::endl; 
+            ros::NodeHandle nh_ = cmd.nh;
+            ros::ServiceClient takeoff_client_ = nh_.serviceClient<std_srvs::Empty>("takeoff");
+            ros::Subscriber odom_sub = nh_.subscribe("ground_truth/odometry", 1, mav_pose_cb_);
+            while(mav_pose_.header.seq != 0){
+                ros::spinOnce();
+            }
+            double start_height = mav_pose_.pose.pose.position.z;
+            double target_height = start_height + 1.0;
+            // might need to relax target_height to account for noise
+            std_srvs::Empty takeoff_msg;
+            if (takeoff_client_.call(takeoff_msg))
+            {
+                std::cout << "Taking off" << std::endl;
+            }
+            else
+            {
+                ROS_ERROR("Failed to takeoff");
+            }
+            while(mav_pose_.pose.pose.position.z < target_height){
+                ros::spinOnce();
+            }
         }
 
         void TrajExec(CmdTrajectory const &) { std::cout << "Exploring" << std::endl; ExploreDone=true;}
-        void GetPkg(CmdGetPkg const &) { std::cout << "Going to LZ" << std::endl; AtLZ=true;}
-        void GotoDrop(CmdGotoDrop const &) { std::cout << "Going to mailbox" << std::endl; AtMailbox=true;}
-        void Ascending(CmdAscent const &) { std::cout << "Ascending" << std::endl; }
+
+        void GetPkg(CmdGetPkg const & cmd) 
+        {
+            ros::NodeHandle nh_ = cmd.nh;
+            ros::Subscriber task_info_sub = nh_.subscribe("task_info", 1000, task_info_cb_);
+            ros::Publisher command_pub = nh_.advertise<geometry_msgs::PoseStamped>("command/pose", 1000);
+            geometry_msgs::PoseStamped command_msg;
+            while(task_info_.loc_type != "Home"){
+                ros::spinOnce();
+            }
+            command_msg.header = task_info_.header;
+            command_msg.pose.position = task_info_.position;
+            int t = 0;
+            // without this loop the topic doesn't seem to catch the command message
+            while(t++<10){
+                command_pub.publish(command_msg);
+            }
+        }
+
+        void GotoDrop(CmdGotoDrop const & cmd) 
+        { 
+            ros::NodeHandle nh_ = cmd.nh;
+            ros::Subscriber task_info_sub = nh_.subscribe("task_info", 1000, task_info_cb_);
+            ros::Publisher command_pub = nh_.advertise<geometry_msgs::PoseStamped>("command/pose", 1000);
+            geometry_msgs::PoseStamped command_msg;
+            while(task_info_.loc_type != "Drop"){
+                ros::spinOnce();
+            }
+            command_msg.header = task_info_.header;
+            command_msg.pose.position = task_info_.position;
+            int t = 0;
+            // without this loop the topic doesn't seem to catch the command message
+            while(t++<10){
+                command_pub.publish(command_msg);
+            }
+            return;
+        }
+
+        void Ascending(CmdAscent const & cmd) 
+        { 
+            ros::NodeHandle nh_ = cmd.nh;
+            // parametrize as hover height
+            double target_height = 4.0;
+            double curr_x, curr_y, curr_z=0;
+            bool AscentDone = false;
+            ros::Rate loopRate(10);
+            ros::Subscriber mav_pose_sub = nh_.subscribe("ground_truth/odometry", 1, mav_pose_cb_);
+            ros::Publisher command_pub = nh_.advertise<geometry_msgs::PoseStamped>("command/pose", 1);
+            geometry_msgs::PoseStamped command_msg;
+            while(curr_z==0)
+            {
+                ros::spinOnce();
+                curr_x = mav_pose_.pose.pose.position.x;
+                curr_y = mav_pose_.pose.pose.position.y;
+                curr_z = mav_pose_.pose.pose.position.z;
+                loopRate.sleep();
+            }
+            command_msg.header = mav_pose_.header;
+            command_msg.pose.position.x = mav_pose_.pose.pose.position.x;
+            command_msg.pose.position.y = mav_pose_.pose.pose.position.y;
+            command_msg.pose.position.z = target_height;
+            int t=0;
+            while(!AscentDone)
+            {
+                if(t++<10){
+                    command_pub.publish(command_msg);
+                }
+                ros::spinOnce();
+                curr_z = mav_pose_.pose.pose.position.z;
+                AscentDone = (curr_z < target_height) ? false : true;
+                loopRate.sleep();
+            }
+            return;
+        }
+
         void Dropping(CmdDrop const &) { std::cout << "Dropping" << std::endl; PkgAttached = false;}
+        
         void Hovering(CmdHover const &) { std::cout << "Hovering" << std::endl; }
-        void Landing(CmdLand const &) { std::cout << "Landing" << std::endl; PkgAttached = true;}
-        // void Resting(CmdRest const &) { std::cout << "Resting" << std::endl; }
-        void Descending(CmdDescent const &) { std::cout << "Descending" << std::endl; }
+        
+        void Landing(CmdLand const &) { 
+            ros::NodeHandle nh_ = cmd.nh;
+            // parametrize as land height
+            double target_height = 0.08;
+            double curr_x, curr_y, curr_z=0;
+            bool LandingDone = false;
+            ros::Rate loopRate(10);
+            ros::Subscriber mav_pose_sub = nh_.subscribe("ground_truth/odometry", 1, mav_pose_cb_);
+            ros::Publisher command_pub = nh_.advertise<geometry_msgs::PoseStamped>("command/pose", 1);
+            geometry_msgs::PoseStamped command_msg;
+            while(curr_z==0)
+            {
+                ros::spinOnce();
+                curr_x = mav_pose_.pose.pose.position.x;
+                curr_y = mav_pose_.pose.pose.position.y;
+                curr_z = mav_pose_.pose.pose.position.z;
+                loopRate.sleep();
+            }
+            command_msg.header = mav_pose_.header;
+            command_msg.pose.position.x = mav_pose_.pose.pose.position.x;
+            command_msg.pose.position.y = mav_pose_.pose.pose.position.y;
+            command_msg.pose.position.z = target_height;
+            int t=0;
+            while(!LandingDone)
+            {
+                if(t++<10){
+                    command_pub.publish(command_msg);
+                }
+                ros::spinOnce();
+                curr_z = mav_pose_.pose.pose.position.z;
+                LandingDone = (curr_z > target_height) ? false : true;
+                loopRate.sleep();
+            }
+            PkgAttached = true;
+            return;
+        }
+        
+        void Descending(CmdDescent const & cmd) 
+        { 
+            ros::NodeHandle nh_ = cmd.nh;
+            // parametrize as drop start height
+            double target_height = 1.0;
+            double curr_x, curr_y, curr_z=0;
+            bool DescentDone = false;
+            ros::Rate loopRate(10);
+            ros::Subscriber mav_pose_sub = nh_.subscribe("ground_truth/odometry", 1, mav_pose_cb_);
+            ros::Publisher command_pub = nh_.advertise<geometry_msgs::PoseStamped>("command/pose", 1);
+            geometry_msgs::PoseStamped command_msg;
+            while(curr_z==0)
+            {
+                ros::spinOnce();
+                curr_x = mav_pose_.pose.pose.position.x;
+                curr_y = mav_pose_.pose.pose.position.y;
+                curr_z = mav_pose_.pose.pose.position.z;
+                loopRate.sleep();
+            }
+            command_msg.header = mav_pose_.header;
+            command_msg.pose.position.x = mav_pose_.pose.pose.position.x;
+            command_msg.pose.position.y = mav_pose_.pose.pose.position.y;
+            command_msg.pose.position.z = target_height;
+            int t=0;
+            while(!DescentDone)
+            {
+                if(t++<10){
+                    command_pub.publish(command_msg);
+                }
+
+                ros::spinOnce();
+                curr_z = mav_pose_.pose.pose.position.z;
+                DescentDone = (curr_z > target_height) ? false : true;
+                loopRate.sleep();
+            }
+            return;      
+        }
+        
         void DropOver(CmdDropOver const &) { std::cout << "Dropped Pkg" << std::endl; }
         
 
-        bool ReachedLZ(CmdHover const &)
+        bool isAtLoc(ros::NodeHandle nh)
         {
-            if(AtLZ){
+            ros::Subscriber mav_pose_sub = nh.subscribe("ground_truth/odometry", 1, mav_pose_cb_);
+            ros::Rate loopRate(10);
+            double curr_pos[3], target_pos[3];
+            bool AtLoc = false;
+
+            while(!AtLoc)
+            {
+                ros::spinOnce();
+                double dist=0;
+
+                curr_pos[0] = mav_pose_.pose.pose.position.x;
+                curr_pos[1] = mav_pose_.pose.pose.position.y;
+                curr_pos[2] = mav_pose_.pose.pose.position.z;
+
+                target_pos[0] = task_info_.position.x;
+                target_pos[1] = task_info_.position.y;
+                target_pos[2] = task_info_.position.z;
+
+                for(int i=0; i<3; i++)
+                {
+                    dist += (curr_pos[i] - target_pos[i])*(curr_pos[i] - target_pos[i]);
+                }
+                
+                AtLoc = (dist > E*E) ? false : true;
+                loopRate.sleep();
+            }
+            
+            if(task_info_.loc_type=="Home")
+            {
+                AtLZ = AtLoc;
+            }
+            else if(task_info_.loc_type=="Drop")
+            {
+                AtMailbox = AtLoc;
+            }
+
+            return AtLoc;
+        }
+
+        bool ReachedLZ(CmdHover const & cmd)
+        {
+    
+            if(isAtLoc(cmd.nh))
+            {
                 std::cout << "Reached LZ" << std::endl;
             }
-            else{
-                std::cout << "Not at LZ yet" << std::endl;
-            }
+
             return AtLZ;
         }
 
-        bool ReachedMailbox(CmdHover const &)
+        bool ReachedMailbox(CmdHover const & cmd)
         {
-            if(AtMailbox){
+            if(isAtLoc(cmd.nh))
+            {
                 std::cout << "Reached Mailbox" << std::endl;
             }
-            else{
-                std::cout << "Not at mailbox yet" << std::endl;
-            }
+
             return AtMailbox;
         }
 
