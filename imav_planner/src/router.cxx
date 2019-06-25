7f64faff4134198c6cdb898eba62a10bd75d070a
Ashwin Shenai <ashwins@iitk.ac.in>
2019-06-25 14:01:51 +0530

    configured router for common message stream

--- imav_planner/src/router.cxx
+++ imav_planner/src/router.cxx
@@ -1,46 +1,34 @@
 #include <ros/ros.h>
+#include <geometry_msgs/Point.h>
 #include <imav_planner/task_info.h>
 #include <detector_msgs/BBPoses.h>
 #include <detector_msgs/BBPose.h>
 
-std::string mav_name;
-std::string names[3];
-detector_msgs::BBPoses objects[3];
-int id = 0, idIndex = 0;
-int types[3] = {-10,-20,-30};
+std::string colour;
+detector_msgs::BBPoses objects;
+int id = 0;
 
-void redCallback(const detector_msgs::BBPoses& msg)
+void objCallback(const detector_msgs::BBPoses& msg)
 {
-    objects[0] = msg;
+    objects = msg;
 }
 
-void yellowCallback(const detector_msgs::BBPoses& msg)
-{
-    objects[1] = msg;
-}
-
-void blueCallback(const detector_msgs::BBPoses& msg)
-{
-    objects[2] = msg;
-}
-
-void publishMsg(std::string mav_name, geometry_msgs::Point position, ros::Publisher *pubPtr)
+void publishMsg(std::string mav_name, geometry_msgs::Point position, ros::Publisher *pubPtr, std::string loc_type)
 {
     imav_planner::task_info msg;
     msg.header.stamp = ros::Time::now();
-    msg.loc_type = "Drop";
-    msg.mavName = mav_name;
+    msg.loc_type = loc_type;
+    msg.mav_name = mav_name;
     msg.position = position;
     pubPtr->publish(msg);
     return;
 }
 
-void createTasks(std::string mav_name, detector_msgs::BBPoses *obj, ros::Publisher *pubPtr)
+void createTasks(detector_msgs::BBPoses *obj, ros::Publisher *pubPtr)
 {
-    for(int i=0; i<obj->object_poses.size(); i++)
+    for(int i=0; i < obj->object_poses.size(); i++)
     {
-        detector_msgs::BBPose pose = obj->object_poses.at(i);
-        if(pose.type == -id) publishMsg(mav_name, pose.position, pubPtr);
+        if(obj->object_poses.at(i).type == id) publishMsg(obj->mav_name, obj->object_poses.at(i).position, pubPtr, "Drop");
     }
     return;
 }
@@ -50,32 +38,20 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "router");
     ros::NodeHandle nh;
 
-    nh.getParam("router/mavName", mav_name);
-    nh.getParam("router/redName", names[0]);
-    nh.getParam("router/yellowName", names[1]);
-    nh.getParam("router/blueName", names[2]);
+    nh.getParam("router/colour", colour);
 
-    for(int i=0; i<3; i++)
-    {
-        if(names[i] == mav_name) 
-        {
-            id = -types[i];
-            i = idIndex;
-            break;
-        }
-    }    
+    if(colour == "red") id = -10;
+    else if(colour == "yellow") id = -20;
+    else if(colour == "blue") id = -30;
 
-    ros::Subscriber redSub = nh.subscribe("red", 10, redCallback);
-    ros::Subscriber yellowSub = nh.subscribe("yellow", 10, yellowCallback);
-    ros::Subscriber blueSub = nh.subscribe("blue", 10, blueCallback);
-
+    ros::Subscriber objSub = nh.subscribe("objects", 10, objCallback);
     ros::Publisher taskPub = nh.advertise<imav_planner::task_info>("task", 1);
     ros::Rate loopRate(10);
 
     while(ros::ok())
     {
-        while(objects[0].imageID < 1 && objects[1].imageID < 1 && objects[2].imageID < 1) ros::spinOnce();
-        for(int i=0; i<3; i++) createTasks(names[(idIndex+i)%3], objects + (idIndex+i)%3, &taskPub);
+        while(objects.imageID < 1) ros::spinOnce();
+        createTasks(&objects, &taskPub);
         ros::spinOnce();
         loopRate.sleep();
     }
